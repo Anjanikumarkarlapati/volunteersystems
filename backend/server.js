a7b1f12397ae6216046c509f8875ec21fb51bacd
abhinay-ds <abhinaypolisetty0@gmail.com>
2026-06-23 10:15:24 +0530

    Fixed CORS issue

--- backend/server.js
+++ backend/server.js
@@ -8,18 +8,7 @@ const activityRoutes = require('./routes/activityRoutes');
 
 connectDB();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Credentials', 'true');
-
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
+app.use(cors());
 
 app.use(express.json());
 
